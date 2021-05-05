16136776ca329ebd994baca89ca55d8349bde84b
Steve Bennett <steveben@fb.com>
2021-05-05 18:03:18 +1000

    init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+.container.mx-auto
+  img.mx-auto(src="./assets/mapswell-logo.png")
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+export default defineComponent({
+    name: 'App',
+    components: {
+        HelloWorld,
+    },
+});
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+
+h1 {
+    background: blue;
+}
+.hello {
+    color: green;
+}
+</style>
